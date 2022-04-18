de5a9fe8e2b7b536dc671b048c8fb9a5c8a91f6c
JJ Escandor <jj.p.escandor@gmail.com>
2022-04-18 00:03:46 -0400

    Finished project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import './App.css';
+import Coin from './Coin';
+
+
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+      .then(res => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch(error => console.log(error.message));
+  }, []);
+
+  const handleChange = e => {
+    setSearch(e.target.value);
+  }
+
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase()))
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="coin-app">
+      <div className='coin-search'>
+        <h1 className='coint-text'>Search a currency</h1>
+        <form >
+          <input type='text' placeholder='Search' className='coin-input'
+            onChange={handleChange} />
+        </form>
+      </div>
+      {filteredCoins.map((coin, idx) => {
+        return (
+          <Coin
+            key={idx}
+            name={coin.name}
+            price={coin.current_price}
+            image={coin.image}
+            volume={coin.total_volume}
+            symbol={coin.symbol}
+            priceChange={coin.price_change_percentage_24h}
+            marketcap={coin.market_cap}
+
+          />
+        )
+      })}
+    </div >
   );
 }
 
